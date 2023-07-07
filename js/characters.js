52f1cd8723bc45478b89e204d32a49379615ec96
EdgarSago <edgar13sago@gmail.com>
2023-07-06 18:14:18 -0600

    cards Characters

--- js/characters.js
+++ js/characters.js
@@ -20,3 +20,27 @@ setInterval(function() {
     }
 },5000);
 
+// CARDS
+const characters = JSON.parse(file);
+console.log(characters.results);
+
+function templateCard(character) {
+    let a = document.createElement('a');
+
+    a.innerHTML = `
+    <a href="" class="box" title="${character.name}">
+        <img class="img" src="${character.img1}" alt="Imagen del personaje: ${character.name}">
+    </a>
+    `
+
+    const card = document.querySelector('.card');
+    card.appendChild(a);
+}
+
+function createCards() {
+    for (let i = 0; i < characters.results.length; i++) {
+        templateCard(characters.results[i]);
+    }
+}
+
+createCards();
